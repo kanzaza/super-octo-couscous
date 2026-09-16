50a658724b8038c30ec9fff794bd2458c27f43c2
kanzaza <gotaog4.kan4@gmail.com>
2026-09-17 01:03:55 +0700

    Add files via upload

---
+++ src/lib/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from '../../firebase-applet-config.json';
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+// Use configured firestoreDatabaseId if provided, else default
+export const db = firebaseConfig.firestoreDatabaseId
+  ? getFirestore(app, firebaseConfig.firestoreDatabaseId)
+  : getFirestore(app);
+
+export default app;
